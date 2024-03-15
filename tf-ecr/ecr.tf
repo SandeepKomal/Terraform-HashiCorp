542f415b8588239ef5155a982da4b61bf3094aaa
SandeepKomal <sandeepkomalp@gmail.com>
2024-03-15 18:14:12 +0530

    ECR updated

--- tf-ecr/ecr.tf
+++ tf-ecr/ecr.tf
@@ -1,15 +1,15 @@
 provider "aws" {
-  region = "us-east-1"  # Set your desired AWS region
+  region = var.aws_region # Set your desired AWS region
 }
 
 resource "aws_ecr_repository" "my_ecr_repo" {
-  name = "psk-ecr-repo"
-  image_tag_mutability = "MUTABLE"  # or "IMMUTABLE" depending on your requirements
+  name = var.ecr_name
+  image_tag_mutability  = var.image_mutability
+  tags             = var.tags
 
-  # Optional settings
   image_scanning_configuration {
     scan_on_push = false
   }
 
-  # You can also configure lifecycle policies, encryption, etc.
+
 }
