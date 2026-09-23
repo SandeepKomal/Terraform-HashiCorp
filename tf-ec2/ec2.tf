b28f19bd33efa8d62909c11b037b503c2b84a91c
SandeepKomal <sandeepkomalp@gmail.com>
2026-09-23 17:52:37 +0530

    style: format tf-ec2/ec2.tf

--- tf-ec2/ec2.tf
+++ tf-ec2/ec2.tf
@@ -2,16 +2,15 @@ provider "aws" {
   region = var.aws_region
 }
 
-#Create security group with firewall rules
-
-
+# Create security group with firewall rules
 resource "aws_instance" "myFirstInstance" {
-  subnet_id = var.subnet_id
-  ami     = var.ami_id
-  key_name = var.key_name
-  instance_type = var.instance_type
-  vpc_security_group_ids = [ var.aws_security_group ]
-  tags= {
+  subnet_id              = var.subnet_id
+  ami                    = var.ami_id
+  key_name               = var.key_name
+  instance_type          = var.instance_type
+  vpc_security_group_ids = [var.aws_security_group]
+
+  tags = {
     Name = "komaltf1 Instance"
   }
-}  
+}
