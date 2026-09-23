f5dddf4c4c664ced1c520bd0a8f3f1c37d09c038
SandeepKomal <sandeepkomalp@gmail.com>
2026-09-23 17:57:53 +0530

    style: format ECS/variables.tf

--- ECS/variables.tf
+++ ECS/variables.tf
@@ -1,44 +1,35 @@
-# variable "aws_access_key" {
-#     description = "The IAM public access key"
-# }
-
-# variable "aws_secret_key" {
-#     description = "IAM secret access key"
-# }
-
 variable "aws_region" {
-    description = "The AWS region things are created in"
+  description = "The AWS region things are created in"
 }
 
 variable "ec2_task_execution_role_name" {
-    description = "ECS task execution role name"
-    default = "myEcsTaskExecutionRole"
+  description = "ECS task execution role name"
+  default     = "myEcsTaskExecutionRole"
 }
 
 variable "ecs_auto_scale_role_name" {
-    description = "ECS auto scale role name"
-    default = "myEcsAutoScaleRole"
+  description = "ECS auto scale role name"
+  default     = "myEcsAutoScaleRole"
 }
 
 variable "az_count" {
-    description = "Number of AZs to cover in a given region"
-    default = "2"
+  description = "Number of AZs to cover in a given region"
+  default     = "2"
 }
 
 variable "app_image" {
-    description = "Docker image to run in the ECS cluster"
-    default     = "nginx:latest"
+  description = "Docker image to run in the ECS cluster"
+  default     = "nginx:latest"
 }
 
 variable "app_port" {
-    description = "Port exposed by the docker image to redirect traffic to"
-    type = number
+  description = "Port exposed by the docker image to redirect traffic to"
+  type        = number
 }
 
 variable "app_count" {
-    description = "Number of docker containers to run"
-    type = number
-    
+  description = "Number of docker containers to run"
+  type        = number
 }
 
 variable "health_check_path" {
@@ -46,28 +37,26 @@ variable "health_check_path" {
 }
 
 variable "fargate_cpu" {
-    description = "Fargate instance CPU units to provision (1 vCPU = 1024 CPU units)"
-    default = "1024"
+  description = "Fargate instance CPU units to provision (1 vCPU = 1024 CPU units)"
+  default     = "1024"
 }
 
 variable "fargate_memory" {
-    description = "Fargate instance memory to provision (in MiB)"
-    default = "2048"
+  description = "Fargate instance memory to provision (in MiB)"
+  default     = "2048"
 }
 
-
 variable "aws_vpc" {
-    description = "vpc id"
-    type = string
+  description = "vpc id"
+  type        = string
 }
 
 variable "aws_subnet" {
-    description = "aws subnets"
-    type = list(string)
+  description = "aws subnets"
+  type        = list(string)
 }
 
 variable "ami_id" {
-    description = "ami"
-    type = string
+  description = "ami"
+  type        = string
 }
-
