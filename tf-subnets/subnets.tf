636bcec4f5c3255cf334d22072d511537446edc5
SandeepKomal <sandeepkomalp@gmail.com>
2026-09-23 18:04:28 +0530

    Fix Terraform formatting in tf-subnets

--- tf-subnets/subnets.tf
+++ tf-subnets/subnets.tf
@@ -7,9 +7,9 @@ resource "aws_vpc" "example" {
 }
 
 resource "aws_subnet" "example_subnet" {
-  vpc_id                  = aws_vpc.example.id
-  cidr_block              = "172.20.0.0/24" # Change this to your desired CIDR block
-  availability_zone       = "us-east-1a"    # Change this to your desired availability zone
+  vpc_id            = aws_vpc.example.id
+  cidr_block        = "172.20.0.0/24" # Change this to your desired CIDR block
+  availability_zone = "us-east-1a" # Change this to your desired availability zone
 
   # Other optional parameters
   map_public_ip_on_launch = true
