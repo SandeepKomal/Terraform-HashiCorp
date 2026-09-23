71cdf5035836c895ec525f472510b74c6e2ee355
SandeepKomal <sandeepkomalp@gmail.com>
2026-09-23 17:52:14 +0530

    style: format ECS-EC2/variables.tf

--- ECS-EC2/variables.tf
+++ ECS-EC2/variables.tf
@@ -1,18 +1,19 @@
 variable "aws_region" {
-    description = "The AWS region things are created in"
+  description = "The AWS region things are created in"
 }
+
 variable "aws_vpc" {
-    description = "vpc id"
-    type = string
+  description = "vpc id"
+  type        = string
 }
 
 variable "aws_subnet" {
-    description = "aws subnets"
-    type = list(string)
+  description = "aws subnets"
+  type        = list(string)
 }
 
 variable "image" {
-    description = "Docker image to run in the ECS cluster"
-    type = string
-    default     = "nginx:latest"
+  description = "Docker image to run in the ECS cluster"
+  type        = string
+  default     = "nginx:latest"
 }
